eee45a9042e57fa59f68277fdb2f15a8680038fc
heegin92 <kimheegin@gmail.com>
2025-07-15 18:19:21 +0900

    스파르타 내용삭제

--- Program.cs
+++ Program.cs
@@ -50,9 +50,7 @@ namespace TextRpg_Comment
         static void DisplayMainUI()
         {
             Console.Clear();
-            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
-            Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
-            Console.WriteLine();
+           
             Console.WriteLine("1. 상태 보기");
             Console.WriteLine("2. 인벤토리");
             Console.WriteLine("3. 상점");
